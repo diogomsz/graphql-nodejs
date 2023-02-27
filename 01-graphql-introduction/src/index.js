09ca64b3db8ae22b1819261a048197c1ce2f2806
Diogo Martins de Assis <diogomartinsdeassis1@gmail.com>
2023-02-27 18:21:34 -0300

    iniciando com resolvers

--- 01-graphql-introduction/src/index.js
+++ 01-graphql-introduction/src/index.js
@@ -4,8 +4,19 @@ const server = new ApolloServer({
   typeDefs: gql`
     type Query {
       hello: String
+      test: String
     }
   `,
+  resolvers: {
+    Query: {
+      hello: () => {
+        return 'Hello World';
+      },
+      test: () => {
+        return 'Just a test';
+      },
+    },
+  },
 });
 
 server.listen(4003).then(({ url }) => {
