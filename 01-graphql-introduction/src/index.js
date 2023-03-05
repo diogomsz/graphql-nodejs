d574e845fd57895f26af53b02cea09929511f147
Diogo Martins de Assis <diogomartinsdeassis1@gmail.com>
2023-03-05 14:37:24 -0300

    trabalhando com object types #2

--- 01-graphql-introduction/src/index.js
+++ 01-graphql-introduction/src/index.js
@@ -3,22 +3,22 @@ import { ApolloServer, gql } from 'apollo-server';
 const server = new ApolloServer({
   typeDefs: gql`
     type Query {
-      id: ID
-      name: String!
-      age: Int
-      height: Float
-      married: Boolean!
-      arrayString: [String!]!
+      user: User
+    }
+
+    type User {
+      id: ID!
+      userName: string
     }
   `,
   resolvers: {
     Query: {
-      id: () => '123id',
-      name: () => 'Diogo Martins de Assis',
-      age: () => 20,
-      height: () => 1.75,
-      married: () => false,
-      arrayString: () => ['Diogo', 'Martins'],
+      user: () => {
+        return {
+          id: 'diogomsz1',
+          userName: 'Diogo Martins de Assis',
+        };
+      },
     },
   },
 });
