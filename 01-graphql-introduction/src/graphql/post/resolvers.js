fc1eb1c9d6b5d2ab4129b9d835fcd6abb0910d31
Diogo Martins de Assis <diogomartinsdeassis1@gmail.com>
2023-03-11 14:46:24 -0300

    renomeando valor do atributo #5

--- 01-graphql-introduction/src/graphql/post/resolvers.js
+++ 01-graphql-introduction/src/graphql/post/resolvers.js
@@ -2,15 +2,15 @@ const posts = () => {
   return [
     {
       id: '1',
-      title: 'Post Title 1',
+      title: 'Post title 1',
     },
     {
       id: '2',
-      title: 'Post Title 2',
+      title: 'Post title 2',
     },
     {
       id: '3',
-      title: 'Post Title 3',
+      title: 'Post title 3',
     },
   ];
 };
@@ -18,7 +18,7 @@ const posts = () => {
 const post = () => {
   return {
     id: '1',
-    title: 'Post Title 1',
+    title: 'Post title 1',
   };
 };
 
